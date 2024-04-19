b8d490c65cc1bf1341de4fd75fd09a428c182438
matheus-oliveira-andrade <mathnhais@outlook.com>
2024-04-19 20:04:25 -0300

    feat: terraform remote state in s3

--- versions.tf
+++ versions.tf
@@ -10,4 +10,10 @@ terraform {
       version = "2.29.0"
     }
   }
+  
+  backend "s3" {
+    bucket = "terraform-state-516735145595"
+    key    = "state/terraform-state.tfstate"
+    region = "us-east-1"
+  }
 }
